30cf1c3c104520cc39d35864d09cf7407ce95fbb
HendaouiYosra <106975299+HendaouiYosra@users.noreply.github.com>
2024-07-08 17:44:29 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,4 +1,4 @@
 def buildApp() {echo "building app........"}
 def testApp() {echo "testing app............"}
-def deployApp() {echo "deploying app...."}
+def deployApp() {echo "deploying app with version ${VERSION}...."}
 return this
